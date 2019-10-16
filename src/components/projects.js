10881c70b4a78a66ff5a6f217c66fd2f30cb3048
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2019-10-16 17:38:32 -0400

    update project

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,37 +6,32 @@ const Projects = props => {
       <h3>My Recent Projects</h3>
       <h5>Here are a few recent works</h5>
       <div id="projects_list">
-        <div id="project">
-          <img src="/pictures/random-acts.png" alt="react" />
-          <div id="title">
-            <a href="https://github.com/buildweek-random-acts-generator/UI-CJ">
-              <h5>Random Acts Generators</h5>
-            </a>
-            <p id="details">fadsfdasfadsfadsfdaf</p>
+        <div class="container">
+          <img src="pictures/random-acts.png" alt="random_act" class="image" />
+          <div class="overlay">
+            <div class="text">Random Act</div>
           </div>
         </div>
-        <div id="project">
-          <img src="/pictures/silent-auction.png" alt="django" />
-          <div id="title">
-            <a href="https://elegant-lamport-acc837.netlify.com/">
-              <h5>Silent Auction</h5>
-            </a>
+        <div class="container">
+          <img
+            src="pictures/silent-auction.png"
+            alt="random_act"
+            class="image"
+          />
+          <div class="overlay">
+            <div class="text">Silent Auction</div>
           </div>
         </div>
-        <div id="project">
-          <img src="/pictures/hooks.png" alt="hooks" />
-          <div id="title">
-            <a href="#">
-              <p>Title</p>
-            </a>
+        <div class="container">
+          <img src="pictures/random-acts.png" alt="random_act" class="image" />
+          <div class="overlay">
+            <div class="text">Hello World</div>
           </div>
         </div>
-        <div id="project">
-          <img src="/pictures/scrapy.png" alt="project" />
-          <div id="title">
-            <a pef="#">
-              <p>Title</p>
-            </a>
+        <div class="container">
+          <img src="pictures/random-acts.png" alt="random_act" class="image" />
+          <div class="overlay">
+            <div class="text">Hello World</div>
           </div>
         </div>
       </div>
